8330431308256bed9c00575abf7698a37f42657f
Bryan Aguilar <baguilar@technisys.com>
2023-12-14 11:45:54 -0500

    feat: Update show results card style

--- src/components/ResultCard.tsx
+++ src/components/ResultCard.tsx
@@ -22,7 +22,7 @@ export const ResultCard: React.FC<ResultCardProps> = (props: ResultCardProps) =>
 	return (
 		<div className="mt-10 grid grid-cols-1 gap-8">
 			{/* result card */}
-			<div className="bg-white rounded-2xl p-8 flex flex-col md:flex-row gap-8 w-full shadow-lg">
+			<div className="border border-white border-dashed p-8 flex flex-col md:flex-row gap-8 w-full">
 				{/* provider detail */}
 				<div className="w-full md:w-[40%]">
 					<div className="flex items-center justify-between">
@@ -34,18 +34,18 @@ export const ResultCard: React.FC<ResultCardProps> = (props: ResultCardProps) =>
 							src={`/images/buses/${transportationProviderLogo}`}
 						/>
 						<div className="text-right">
-							<h3 className="text-xl text-neutral-500 mb-2 font-semibold">$ {price}</h3>
-							<p className="text-neutral-400">{distance} km aprox</p>
+							<h3 className="text-xl text-white mb-2 font-semibold">$ {price}</h3>
+							<p className="text-neutral-50">{distance} km aprox</p>
 						</div>
 					</div>
 					<div className="mt-4">
-						<h2 className="text-xl flex items-center gap-4 mb-2">
+						<h2 className="text-xl flex items-center gap-4 mb-2 text-neutral-100">
 							{transportationProvider}
-							<span className="text-xs py-1 px-2 bg-rose-500 text-white font-bold rounded-md">
+							<span className="text-xs py-1 px-2 border border-darkNavy text-darkNavy font-bold rounded-md">
 								{estimatedTravelTime} hours aprox
 							</span>
 						</h2>
-						<p className="text-neutral-500">{transportationProviderDetails}</p>
+						<p className="text-neutral-200">{transportationProviderDetails}</p>
 					</div>
 				</div>
 				{/* schedules */}
@@ -53,7 +53,7 @@ export const ResultCard: React.FC<ResultCardProps> = (props: ResultCardProps) =>
 					{schedules.map((schedule) => (
 						<div
 							key={schedule}
-							className="rounded-2xl border border-rose-500 p-3 text-rose-500 text-md flex items-center justify-center"
+							className="rounded-2xl border border-darkNavy p-3 text-darkNavy text-md flex items-center justify-center"
 						>
 							<div className="text-md font-bold">{schedule}</div>
 						</div>
