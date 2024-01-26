68edabf5cdf58d5fe80cf9ae3e38f15e66f04401
Bryan Aguilar <baguilar@technisys.com>
2024-01-26 14:53:25 -0500

    test: Add Bancor deepLink

--- src/components/Navbar/UserMenu.tsx
+++ src/components/Navbar/UserMenu.tsx
@@ -29,7 +29,7 @@ export const UserMenu = () => {
 					href={
 						'https://pdd.bancor.com.ar/modo/pagar?qr=00020101021226580024ar.com.modo.decidir-plus012622NRHZ04Q0834TQF4TJM0KTC87520459695303032540576.835802AR5918Demo-MODO3-Preprod6004CABA62840308920024800522saekYLCF5gKYgXYKDVwvNZ0708920024800811TIENDA_NUBE100899999999110352280380024ar.com.modo.decidir-plus0106online81380024ar.com.modo.decidir-plus010100201063045028&callback=https%3A%2F%2Fdemomodo3.mitiendanube.com%2Fcheckout%2Fv3%2Fnext%2F1425373060%2Fb7bf332023af98587af303232a69c84efc081796&callbackSuccess=https%3A%2F%2Fdemomodo3.mitiendanube.com%2Fcheckout%2Fv3%2Fsuccess%2F1425373060%2Fb7bf332023af98587af303232a69c84efc081796'
 					}
-					className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full border border-dashed  transition cursor-pointer text-light hover:text-darkNavy hover:border-darkNavy hover:border-solid"
+					className="text-sm font-semibold py-3 px-4 rounded-full border border-dashed  transition cursor-pointer text-light hover:text-darkNavy hover:border-darkNavy hover:border-solid"
 				>
 					Go to Bancor
 				</Link>
@@ -41,7 +41,6 @@ export const UserMenu = () => {
 					<div className="hidden md:block">
 						<Avatar />
 					</div>
-					L
 				</div>
 			</div>
 			{isMenuOpen && (
