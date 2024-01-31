a3f47b03094fd144cb35a2352d69af26cfcf8b7c
Bryan Aguilar <baguilar@technisys.com>
2024-01-30 22:14:38 -0500

    feat: Remove test deep link

--- src/components/Navbar/UserMenu.tsx
+++ src/components/Navbar/UserMenu.tsx
@@ -6,7 +6,6 @@ import { AiOutlineMenu } from 'react-icons/ai';
 import { MenuItem } from './MenuItem';
 import { useLoginModalStore, useRegisterModalStore } from '../../store';
 import { Avatar } from '../Avatar';
-import Link from 'next/link';
 
 export const UserMenu = () => {
 	const registerModalStore = useRegisterModalStore();
@@ -22,17 +21,9 @@ export const UserMenu = () => {
 	return (
 		<div className="relative">
 			<div className="flex flex-row items-center gap-3">
-				{/* <div className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full border border-dashed  transition cursor-pointer text-light hover:text-darkNavy hover:border-darkNavy hover:border-solid">
+				<div className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full border border-dashed  transition cursor-pointer text-light hover:text-darkNavy hover:border-darkNavy hover:border-solid">
 					Test
-				</div> */}
-				<Link
-					href={
-						'https://pdd.bancor.com.ar/modo/pagar?qr=00020101021226580024ar.com.modo.decidir-plus012622NRHZ04Q0834TQF4TJM0KTC87520459695303032540576.835802AR5918Demo-MODO3-Preprod6004CABA62840308920024800522saekYLCF5gKYgXYKDVwvNZ0708920024800811TIENDA_NUBE100899999999110352280380024ar.com.modo.decidir-plus0106online81380024ar.com.modo.decidir-plus010100201063045028&callback=https%3A%2F%2Fdemomodo3.mitiendanube.com%2Fcheckout%2Fv3%2Fnext%2F1425373060%2Fb7bf332023af98587af303232a69c84efc081796&callbackSuccess=https%3A%2F%2Fdemomodo3.mitiendanube.com%2Fcheckout%2Fv3%2Fsuccess%2F1425373060%2Fb7bf332023af98587af303232a69c84efc081796'
-					}
-					className="text-sm font-semibold py-3 px-4 rounded-full border border-dashed  transition cursor-pointer text-light hover:text-darkNavy hover:border-darkNavy hover:border-solid"
-				>
-					Go to Bancor
-				</Link>
+				</div>
 				<div
 					onClick={toggleOpen}
 					className="p-4 md:py-1 md:px-2 boder-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition"
