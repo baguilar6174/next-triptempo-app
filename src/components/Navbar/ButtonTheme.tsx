f50560c2c4c7a0db30f133e006809624f83b0ffb
Bryan Aguilar <bryan.aguilar6174@gmail.com>
2024-05-18 18:09:56 -0500

    fix: Use right property to set theme

--- src/components/Navbar/ButtonTheme.tsx
+++ src/components/Navbar/ButtonTheme.tsx
@@ -5,16 +5,35 @@ import { Moon, Sun } from 'lucide-react';
 import { useTheme } from 'next-themes';
 
 import { Button } from '@/components/ui/button';
+import Image from 'next/image';
 
 export function ButtonTheme(): JSX.Element {
-	const { setTheme, theme } = useTheme();
+	const [mounted, setMounted] = React.useState(false);
+	const { setTheme, resolvedTheme } = useTheme();
+
+	React.useEffect(() => {
+		setMounted(true);
+	}, []);
+
+	if (!mounted)
+		return (
+			<Image
+				src="data:image/svg+xml;base64,PHN2ZyBzdHJva2U9IiNGRkZGRkYiIGZpbGw9IiNGRkZGRkYiIHN0cm9rZS13aWR0aD0iMCIgdmlld0JveD0iMCAwIDI0IDI0IiBoZWlnaHQ9IjIwMHB4IiB3aWR0aD0iMjAwcHgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHJlY3Qgd2lkdGg9IjIwIiBoZWlnaHQ9IjIwIiB4PSIyIiB5PSIyIiBmaWxsPSJub25lIiBzdHJva2Utd2lkdGg9IjIiIHJ4PSIyIj48L3JlY3Q+PC9zdmc+Cg=="
+				width={36}
+				height={36}
+				sizes="36x36"
+				alt="Loading Light/Dark Toggle"
+				priority={false}
+				title="Loading Light/Dark Toggle"
+			/>
+		);
 
 	return (
 		<Button
 			variant="outline"
 			size="icon"
 			onClick={() => {
-				setTheme(theme === 'light' ? 'dark' : 'light');
+				setTheme(resolvedTheme === 'light' ? 'dark' : 'light');
 			}}
 		>
 			<Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
