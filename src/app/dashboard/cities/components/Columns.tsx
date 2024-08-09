6bf1f1a5e4107557fb939c510bb35cbbde1ee05b
Bryan Aguilar <bryan.aguilar6174@gmail.com>
2024-08-09 12:58:24 -0500

    🧹 chore: Add modal to confirm remove a record in table

--- src/app/dashboard/cities/components/Columns.tsx
+++ src/app/dashboard/cities/components/Columns.tsx
@@ -6,6 +6,15 @@ import { ArrowUpDown, Pencil, Trash } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { type City } from '@/core/entities';
 import { useCitiesStore } from '@/stores/city.store';
+import {
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger
+} from '@/components/ui/dialog';
+import React from 'react';
 
 export const columns: Array<ColumnDef<City>> = [
 	{
@@ -50,6 +59,8 @@ interface TableActionsProps {
 export const TableActions = (props: TableActionsProps): JSX.Element => {
 	const { id } = props;
 
+	const [openModal, setOpenModal] = React.useState<boolean>(false);
+
 	const deleteCity = useCitiesStore((state) => state.delete);
 
 	return (
@@ -57,15 +68,37 @@ export const TableActions = (props: TableActionsProps): JSX.Element => {
 			<Button size="icon">
 				<Pencil className="h-4 w-4" />
 			</Button>
-			<Button
-				variant="destructive"
-				size="icon"
-				onClick={async () => {
-					await deleteCity(id);
-				}}
-			>
-				<Trash className="h-4 w-4" />
-			</Button>
+			<Dialog open={openModal} onOpenChange={setOpenModal}>
+				<DialogTrigger asChild>
+					<Button variant="destructive" size="icon">
+						<Trash className="h-4 w-4" />
+					</Button>
+				</DialogTrigger>
+				<DialogContent className="sm:max-w-[625px]">
+					<DialogHeader>
+						<DialogTitle>Remove city</DialogTitle>
+						<DialogDescription>Are you sure you want to remove this city?</DialogDescription>
+					</DialogHeader>
+					<div className="flex justify-end gap-x-2">
+						<Button
+							variant="outline"
+							onClick={() => {
+								setOpenModal(false);
+							}}
+						>
+							Cancel
+						</Button>
+						<Button
+							onClick={async () => {
+								await deleteCity(id);
+								setOpenModal(false);
+							}}
+						>
+							Continue
+						</Button>
+					</div>
+				</DialogContent>
+			</Dialog>
 		</div>
 	);
 };
